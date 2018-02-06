64c9e41e4be89ab42a0c18083296c5510041dc26
Michael Tashker <tashkermc@gmail.com>
2018-02-05 20:20:05 -0800

    Initial git version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Threading;
+using iSearch;
+
+namespace iSearch
+{
+	public partial class MainWindow
+	{
+		private bool SearchUnavailable;
+		private readonly System.Timers.Timer keyTimer;
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			DispatcherTimer minuteTimer = new DispatcherTimer();
+			minuteTimer.Tick += minuteTimer_Tick;
+			minuteTimer.Interval = new TimeSpan(0, 1, 0);
+			minuteTimer.Start();
+
+			//keyTimer exists to prevent multiple searches within [interval]
+			keyTimer = new System.Timers.Timer(100) {AutoReset = false};
+			keyTimer.Elapsed += keyTimer_Elapsed;
+
+			ShowTime();
+		}
+
+		private void OnExitClick(object sender, RoutedEventArgs e)
+		{
+			Close();
+		}
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			int offsetHeight = 70;
+			int offsetWidth = (int)Width + 0;
+			Left = (int)SystemParameters.PrimaryScreenWidth - offsetWidth;
+			Top = (int)SystemParameters.PrimaryScreenHeight - offsetHeight;
+
+			//following code (along with code at bottom of file) inhibits showing in task switcher
+			WindowInteropHelper wndHelper = new WindowInteropHelper(this);
+			int exStyle = (int)GetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE);
+			exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+			SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+		}
+		
+		private void OnKeyDown(object sender, KeyEventArgs e)
+		{
+			if (SearchUnavailable)
+				return;
+			if (e.Key != Key.Return) return;
+			SearchUnavailable = true;
+			keyTimer.Start();
+			aSearch.Search(tbSearchBox.Text, e.KeyboardDevice.IsKeyDown(Key.LeftCtrl));
+		}
+
+		private void tbSearchBox_PreviewKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Down || e.Key == Key.Up)
+			{
+				tbSearchBox.Text = aSearch.GetStringFromHistory(e);
+			}
+		}
+
+		private void DragWindow(object sender, MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+
+		private void minuteTimer_Tick(object sender, EventArgs e)
+		{
+			if (!tbSearchBox.IsKeyboardFocused)
+				ShowTime();
+		}
+
+		private void keyTimer_Elapsed(object sendeer, EventArgs e)
+		{
+			SearchUnavailable = false;
+		}
+
+		private void ShowTime()
+		{
+			DateTime dt = DateTime.Now.ToLocalTime();
+			tbSearchBox.Text = String.Format("{0}  {1}", dt.ToShortDateString(), dt.ToShortTimeString());
+		}
+
+		#region focus events
+		private void tbSearchBox_MouseLeave(object sender, MouseEventArgs e)
+		{
+			ShowTime();
+		}
+
+		private void tbSearchBox_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+			{
+				tbSearchBox.Text = "";
+				//e.Handled = true;
+			}
+		}
+		#endregion
+
+		private void tbSearchBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			thePopup.IsOpen = true;
+		}
+
+
+		#region calendar
+		private void OnDateClick(object sender, SelectionChangedEventArgs e)
+		{
+			thePopup.IsOpen = false;
+		}
+
+		private void OnPopupClosed(object sender, EventArgs eventArgs)
+		{
+			ShowTime();
+			//DateTime sel = theCalendar.SelectedDate ?? DateTime.Today;
+			//textBox1.Text = sel.ToShortDateString();
+		}
+
+		/*
+		private void OnCalendarMenuClick(object sender, RoutedEventArgs e)
+		{
+			thePopup.IsOpen = true;
+		}
+		 */
+		#endregion
+
+
+		//Following code found in
+		//http://stackoverflow.com/questions/357076/best-way-to-hide-a-window-from-the-alt-tab-program-switcher/551847#551847
+		//posting by Franci Penov 
+
+		#region Window styles
+		[Flags]
+		public enum ExtendedWindowStyles
+		{
+			// ...
+			WS_EX_TOOLWINDOW = 0x00000080,
+			// ...
+		}
+
+		public enum GetWindowLongFields
+		{
+			// ...
+			GWL_EXSTYLE = (-20),
+			// ...
+		}
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+		public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+		{
+			int error = 0;
+			IntPtr result = IntPtr.Zero;
+			// Win32 SetWindowLong doesn't clear error on success
+			SetLastError(0);
+
+			if (IntPtr.Size == 4)
+			{
+				// use SetWindowLong
+				Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+				error = Marshal.GetLastWin32Error();
+				result = new IntPtr(tempResult);
+			}
+			else
+			{
+				// use SetWindowLongPtr
+				result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+				error = Marshal.GetLastWin32Error();
+			}
+
+			if ((result == IntPtr.Zero) && (error != 0))
+			{
+				throw new System.ComponentModel.Win32Exception(error);
+			}
+
+			return result;
+		}
+
+		[DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+		private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+		[DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+		private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+		private static int IntPtrToInt32(IntPtr intPtr)
+		{
+			return unchecked((int)intPtr.ToInt64());
+		}
+
+		[DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+		public static extern void SetLastError(int dwErrorCode);
+
+
+		#endregion
+	}
+}
---
+++ iSearch.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace iSearch
+{
+    public static class aSearch
+    {
+        private static readonly Dictionary<string, Tuple<string,string>> SearchDic =
+            new Dictionary<string, Tuple<string,string>>();
+        private static readonly List<Tuple<string,bool>> History=new List<Tuple<string,bool>>();
+        private static int HistoryPointer;
+
+        static aSearch()
+        {
+            InitDic();
+        }
+
+        public static void Search(string InputString, bool ControlKeyDown)
+        {
+            string Target = "iexplore.exe";
+            string Param;
+            History.Add(new Tuple<string, bool>(InputString, ControlKeyDown));
+            if (InputString.Contains(".") && !InputString.Contains(" "))
+                Param = httpDirect(InputString);
+            else if (ControlKeyDown)
+                Param = httpExpand(InputString);
+            else
+            {
+                int IndexOfSpace = InputString.IndexOf(' ');
+                string key = IndexOfSpace >= 0 ? InputString.Substring(0, IndexOfSpace) : "";
+                Target = SearchDic.ContainsKey(key)
+                    ? FullSearchString(key,InputString.Substring(IndexOfSpace + 1),SearchDic[key].Item2, out Param)
+                        : FullSearchString("gg",InputString,SearchDic["gg"].Item2, out Param);
+            }
+            if (Target != "test")
+                Process.Start(Target, Param);
+        }
+
+        public static string GetStringFromHistory(KeyEventArgs e)
+        {
+            if (History.Count == 0)
+                return string.Empty;
+            if (e.Key == Key.Up)
+                HistoryPointer = HistoryPointer == History.Count - 1 ? HistoryPointer : HistoryPointer + 1;
+            else if (e.Key == Key.Down)
+                HistoryPointer = HistoryPointer == 0 ? 0 : HistoryPointer - 1;
+            return History[HistoryPointer].Item1;
+        }
+
+        #region Search Providers and Dictionary Initialization
+
+        /*
+        Initialize dictionary of search providers from application settings.
+        We're looking for strings whose Name starts with Search and are of the type
+        Shortcut,Target,Separator
+        */
+        private static void InitDic()
+        {
+            foreach (SettingsProperty currentProperty in Properties.Settings.Default.Properties)
+            {
+                if (currentProperty.Name.StartsWith("SEARCH"))
+                {
+                    //parse
+                    string[] values = currentProperty.DefaultValue.ToString().Split(',');
+                    var key = values[0];
+                    var target = values[1];
+                    var separator = values[2];
+                    SearchDic.Add(key, new Tuple<string, string>(target, separator));
+                }
+            }
+        }
+
+        private static string FullSearchString(string key, string SearchInput, string FormatInfo, out string Param)
+        {
+            string target = SearchDic[key].Item1;
+            Param = string.Empty;
+            string[] parts = FormatInfo.Split('=');
+            string FormatKey = parts[0];
+            string FormatVal = parts[1];
+            switch (FormatKey)
+            {
+                case "SEP":
+                    if (FormatVal.Length == 1)
+                        return target+SearchInput.Replace(' ', FormatVal[0]);
+                    return target+SearchInput.Replace(" ", FormatVal);
+                case "PARM":
+                    Param=FormatVal.Replace("$", SearchInput);
+                    return target;
+            }
+            return string.Empty;
+        }
+
+        private static string httpExpand(string srch)
+        {
+            //return ("http://www." + srch + ".com");
+            //return ("https://" + srch + ".com");
+            return httpDirect(srch) + ".com";
+        }
+
+        private static string httpDirect(string srch)
+        {
+            //return ("http://" + srch);
+            //return ("https://" + srch);
+            return (srch);
+        }
+
+        #endregion
+
+        #region obso
+        /*
+            Initialize dictionary of search providers from application settings.
+            We're looking for strings of the type
+            Shortcut[i], Prefix[i], Separator[i]
+            where i ranges up sequentially from 1 without gap
+        */
+        /*
+        private static void InitDicObso()
+        {
+            string key, prefix, separator;
+            int i = 1;
+            while (true)
+            {
+                try
+                {
+                    key = (string)Properties.Settings.Default["Shortcut" + i];
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+                try
+                {
+                    prefix = (string)Properties.Settings.Default["Prefix" + i];
+                    separator = (string)Properties.Settings.Default["Separator" + i];
+                }
+                catch (Exception)
+                {
+                    return;
+                }
+                SearchDic.Add(key, new Tuple<string, string>(prefix, separator));
+                i++;
+            }
+        }
+        */
+
+        /*
+        SearchDic.Add("gg", googleSearch);
+        SearchDic.Add("amaz", amazSearch);
+        SearchDic.Add("map", mapSearch);
+        SearchDic.Add("wik", wikSearch);
+        SearchDic.Add("imdb", imdbSearch);
+        SearchDic.Add("test", testSearch);
+        SearchDic.Add("so", soSearch);
+        */
+
+        /*
+        private static string mapSearch(string srch)
+        {
+            const string Prefix = "https://www.google.com/maps/search/";
+            return (Prefix + srch.Replace(' ', '+'));
+        }
+
+        private static string testSearch(string srch)
+        {
+            return "test";
+        }
+
+        private static string wikSearch(string srch)
+        {
+            const string Prefix = "http://www.wikipedia.org/wiki/Special:Search/";
+            return (Prefix + srch.Replace(' ', '_'));
+        }
+
+        private static string soSearch(string srch)
+        {
+            const string Prefix = "https://www.stackoverflow.com/search?q=";
+            return (Prefix + srch.Replace(' ', '+'));
+        }
+
+        private static string googleSearch(string srch)
+        {
+            const string Prefix = "https://www.google.com/search?q=";
+            return (Prefix + srch.Replace(' ', '+'));
+        }
+
+        private static string imdbSearch(string srch)
+        {
+            const string Prefix = "http://www.imdb.com/find/?q=";
+            return (Prefix + srch.Replace(' ', '+'));
+        }
+
+        private static string amazSearch(string srch)
+        {
+            const string Prefix = "http://www.amazon.com/s/?keywords=";
+            return (Prefix + srch.Replace(" ", "%20"));
+        }
+        */
+        #endregion
+
+    }
+}
