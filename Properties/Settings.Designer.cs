f30381437c6d5ccd9dcfddf1662e5fe96077142f
Mike Tashker <tashkermc@gmail.com>
2019-04-09 14:17:20 -0700

    fixed local (e.g. everything) search when search string contained .exe

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace iSearch.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -89,7 +89,7 @@ namespace iSearch.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("ev,c:\\program files\\everything\\everything.exe,c:\\program files\\everything\\everyth" +
-            "ing.exe -s $,")]
+            "ing.exe -s \"$\", ")]
         public string SEARCHev {
             get {
                 return ((string)(this["SEARCHev"]));
--- iSearch.cs
+++ iSearch.cs
@@ -7,79 +7,88 @@ using iSearch.Properties;
 
 namespace iSearch
 {
-	public static class aSearch
-	{
-		private static readonly Dictionary<string, SearchItem> SearchDic =
-			new Dictionary<string, SearchItem>();
-		private static readonly List<Tuple<string, bool>> History = new List<Tuple<string, bool>>();
-		private static int HistoryPointer;
-		private static readonly string Browser;
+    public static class aSearch
+    {
+        private static readonly Dictionary<string, SearchItem> SearchDic =
+            new Dictionary<string, SearchItem>();
 
-		static aSearch()
-		{
-			InitSearchDic();
-			//Browser is either set to some executable or null
-			try
-			{
-				Browser = Settings.Default["Browser"].ToString();
-			}
-			catch (Exception)
-			{
-				// ignored
-			}
-		}
+        private static readonly List<Tuple<string, bool>> History = new List<Tuple<string, bool>>();
+        private static int HistoryPointer;
+        private static readonly string Browser;
 
-		public static void Search(string InputString, bool ControlKeyDown)
-		{
-			string URI;
-			History.Add(new Tuple<string, bool>(InputString, ControlKeyDown));
- 
-			if (InputString.Contains(".") && !InputString.Contains(" "))
-			    URI = httpDirect(InputString);    //this will send the string directly to the browser
-			else if (ControlKeyDown)
-			    URI = httpExpand(InputString);    //this adds .com and sends to browser
-			else
-			{
-				int IndexOfSpace = InputString.IndexOf(' ');
-				if (IndexOfSpace < 0) //check for single token input
-				{
-				    /*
-					 * Single token here
-					 * If it's in the search dictionary, just go to that domain.
-					 * Else google the token
-					 */
-				    URI = SearchDic.ContainsKey(InputString) ? SearchDic[InputString].Site : SearchDic["gg"].ReplaceDollar(InputString);
-				}
-				else
-				{
-				    //token has a space in it
-					string key = InputString.Substring(0, IndexOfSpace);    //key is first token
-					/*
-					 * If it's in the search dictionary, create target and parm
-					 * param is always the parameter.
-					 * target is only set if it's an internal program, else empty string
-					 */
-				    URI = SearchDic.ContainsKey(key) ? SearchDic[key].ReplaceDollar(InputString.Substring(IndexOfSpace+1)) : SearchDic["gg"].ReplaceDollar(InputString);
-				}
+        static aSearch()
+        {
+            InitSearchDic();
+            //Browser is either set to some executable or null
+            try
+            {
+                Browser = Settings.Default["Browser"].ToString();
             }
-            if (URI.Contains(".exe"))
-                RunLocal(URI);
-            else
-                RunBrowser(URI);
-		}
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
 
-	    static void RunLocal(string target)
-	    {
-	        if (target.ToLower().EndsWith(".exe"))
-	            Process.Start(target);
-	        else
-	        {
-	            int Index = target.IndexOf(".exe", StringComparison.OrdinalIgnoreCase);
-	            string param = target.Substring(Index+5);
-	            target = target.Substring(0, Index+4);
-	            Process.Start(target, param);
-	        }
-	    }
+        public static void Search(string InputString, bool ControlKeyDown)
+        {
+            string URI;
+            History.Add(new Tuple<string, bool>(InputString, ControlKeyDown));
+
+            if (InputString.Contains(".") && !InputString.Contains(" "))
+                URI = httpDirect(InputString); //this will send the string directly to the browser
+            else if (ControlKeyDown)
+                URI = httpExpand(InputString); //this adds .com and sends to browser
+            else
+            {
+                int IndexOfSpace = InputString.IndexOf(' ');
+                if (IndexOfSpace < 0) //check for single token input
+                {
+                    /*
+                     * Single token here
+                     * If it's in the search dictionary, just go to that domain.
+                     * Else google the token
+                     */
+                    URI = SearchDic.ContainsKey(InputString)
+                        ? SearchDic[InputString].Site
+                        : SearchDic["gg"].ReplaceDollar(InputString);
+                }
+                else
+                {
+                    //token has a space in it
+                    string key = InputString.Substring(0, IndexOfSpace); //key is first token
+                    /*
+                     * If it's in the search dictionary, create target and parm
+                     * param is always the parameter.
+                     * target is only set if it's an internal program, else empty string
+                     */
+                    URI = SearchDic.ContainsKey(key)
+                        ? SearchDic[key].ReplaceDollar(InputString.Substring(IndexOfSpace + 1))
+                        : SearchDic["gg"].ReplaceDollar(InputString);
+                }
+            }
+
+            if (URI.Contains("http"))
+                RunBrowser(URI);
+            else
+                Process.Start(GetTarget(URI), GetParm(URI));
+        }
+
+        #region URI manipulators
+        static string GetTarget(string uri)
+        {
+            int Index = uri.IndexOf(".exe", StringComparison.OrdinalIgnoreCase);
+            return uri.Substring(0, Index + 4);
+        }
+
+        static string GetParm(string uri)
+        {
+            int Index = uri.IndexOf(".exe", StringComparison.OrdinalIgnoreCase);
+            if (Index + 5 < uri.Length)
+                return uri.Substring(Index + 5);
+            return string.Empty;
+        }
+        #endregion
 
         static void RunBrowser(string target)
         {
@@ -89,22 +98,22 @@ namespace iSearch
                 Process.Start(target);
         }
 
-		public static string GetStringFromHistory(KeyEventArgs e)
-		{
-			if (History.Count == 0)
-				return string.Empty;
-			if (e.Key == Key.Up)
-				HistoryPointer = HistoryPointer == History.Count - 1 ? HistoryPointer : HistoryPointer + 1;
-			else if (e.Key == Key.Down)
-				HistoryPointer = HistoryPointer == 0 ? 0 : HistoryPointer - 1;
-			return History[HistoryPointer].Item1;
-		}
+        public static string GetStringFromHistory(KeyEventArgs e)
+        {
+            if (History.Count == 0)
+                return string.Empty;
+            if (e.Key == Key.Up)
+                HistoryPointer = HistoryPointer == History.Count - 1 ? HistoryPointer : HistoryPointer + 1;
+            else if (e.Key == Key.Down)
+                HistoryPointer = HistoryPointer == 0 ? 0 : HistoryPointer - 1;
+            return History[HistoryPointer].Item1;
+        }
 
-		#region Search Providers and Dictionary Initialization
+        #region Search Providers and Dictionary Initialization
 
-		private static void InitSearchDic()
-		{
-		    /*
+        private static void InitSearchDic()
+        {
+            /*
             Initialize dictionary of search providers from application settings.
             We're looking for strings whose Name starts with Search and are of the type
             Shortcut (i.e. key, Target (often domain URL), SearchString, Separator
@@ -113,53 +122,75 @@ namespace iSearch
             spaces are converted to the separator string
             */
 
-			// You have to prime the actual values pump by asking for one by name.
+            // You have to prime the actual values pump by asking for one by name.
             var unused = Settings.Default.SEARCHdic;
-			foreach (SettingsPropertyValue currentProperty in Settings.Default.PropertyValues)
-			{
-				if (currentProperty.Name.StartsWith("SEARCH"))
-				{
-					/*
-					 * parse: sample is
-					 * imdb,http://www.imdb.com,/find/?q=,SEP=+
-					 */
-					string[] values = currentProperty.PropertyValue.ToString().Split(',');
-					var key = values[0];
-					var site = values[1];
-					var searchstring = values[2];
-					var separator = values[3];
-					SearchDic.Add(key, new SearchItem(site,searchstring,separator));
-				}
-			}
-		}
+            foreach (SettingsPropertyValue currentProperty in Settings.Default.PropertyValues)
+            {
+                if (currentProperty.Name.StartsWith("SEARCH"))
+                {
+                    /*
+                     * parse: sample is
+                     * imdb,http://www.imdb.com,/find/?q=,SEP=+
+                     */
+                    string[] values = currentProperty.PropertyValue.ToString().Split(',');
+                    var key = values[0];
+                    var site = values[1];
+                    var searchstring = values[2];
+                    var separator = values[3];
+                    SearchDic.Add(key, new SearchItem(site, searchstring, separator));
+                }
+            }
+        }
 
-		private static string httpExpand(string srch)
-		{
-			return httpDirect(srch) + ".com";
-		}
+        private static string httpExpand(string srch)
+        {
+            return httpDirect(srch) + ".com";
+        }
 
-		private static string httpDirect(string srch)
-		{
-			return (srch);
-		}
-		#endregion
-	}
+        private static string httpDirect(string srch)
+        {
+            return (srch);
+        }
+
+        #endregion
+    }
 
     internal class SearchItem
     {
         internal SearchItem(string site, string searchstring, string separator)
         {
-            Site = site;
+            Site = site.ToLower();
             SearchString = searchstring;
             Separator = separator;
         }
+
         internal string ReplaceDollar(string parms)
         {
             string input = parms.Replace(" ", Separator);
-            return SearchString.Replace("$",input);
+            return SearchString.Replace("$", input);
         }
+
+        internal bool IsLocal()
+        {
+            return Site.EndsWith(".exe");
+        }
+
+        internal string Target()
+        {
+            int Index = SearchString.IndexOf(".exe", StringComparison.OrdinalIgnoreCase);
+            return SearchString.Substring(0, Index + 4);
+        }
+
+        internal string Parm()
+        {
+            int Index = SearchString.IndexOf(".exe", StringComparison.OrdinalIgnoreCase);
+            if (Index + 5 < SearchString.Length)
+                return SearchString.Substring(Index + 5);
+            return string.Empty;
+        }
+
         internal string Site { get; }
         private string SearchString { get; }
-        private string Separator { get;}
+        private string Separator { get; }
     }
 }
