f79b2693cd91737af11266c69b563edf34d5a6ba
Mike Tashker <tashkermc@gmail.com>
2020-02-05 13:40:37 -0800

    Version number change to 2.4.*

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MGT Systems")]
 [assembly: AssemblyProduct("Floater")]
-[assembly: AssemblyCopyright("Copyright © MGT Systems 2014-2019")]
+[assembly: AssemblyCopyright("Copyright © MGT Systems 2014-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.3.*")]
+[assembly: AssemblyVersion("2.4.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
